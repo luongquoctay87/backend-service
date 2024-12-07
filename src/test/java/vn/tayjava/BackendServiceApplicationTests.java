c465f476b801b20c30b44c19499a18628fbb722a
Lương Quốc Tây <luongquoctay87@gmail.com>
2024-12-07 10:54:06 +0700

    add ingration test

--- src/test/java/vn/tayjava/BackendServiceApplicationTests.java
+++ src/test/java/vn/tayjava/BackendServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package vn.tayjava;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import vn.tayjava.controller.HelloController;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BackendServiceApplicationTests {
 
+	@Autowired
+	private HelloController helloController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(helloController).isNotNull();
 	}
 
 }
