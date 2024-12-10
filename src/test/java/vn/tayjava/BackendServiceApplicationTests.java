40cb83c07f1cdfde0e172851c6add48ae266627e
Lương Quốc Tây <luongquoctay87@gmail.com>
2024-12-10 15:55:23 +0700

    update unit test

--- src/test/java/vn/tayjava/BackendServiceApplicationTests.java
+++ src/test/java/vn/tayjava/BackendServiceApplicationTests.java
@@ -2,15 +2,12 @@ package vn.tayjava;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import vn.tayjava.controller.AuthenticationController;
 import vn.tayjava.controller.EmailController;
 import vn.tayjava.controller.UserController;
 
-@ExtendWith(SpringExtension.class) // kích hoạt sử dụng SpringExtension
 @SpringBootTest
 class BackendServiceApplicationTests {
 
